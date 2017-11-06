335a0943da3e356b200841d3ed64dcebf2eebc5c
shengjun-zh <zhu_sj@126.com>
2017-11-06 11:02:11 +0800

    add debug log

--- sh/setup.sh
+++ sh/setup.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/expect
 
-#spawn psm setup
-#expect {
-#        "Username:" {send "cloud.admin\r"; exp_continue}
-#		"Password:" {send "gReEDy@1Blow\r"; exp_continue}
-#		"Retype Password:" {send "gReEDy@1Blow\r"; exp_continue}
-#		"Identity domain:" {send "gse00002265\r"; exp_continue}
-#       "Region" {send "emea\r"; exp_continue}
-#		"Output format:" {send "\r"}
-#}
-#sexpect eof
-
-psm update
+spawn psm setup
+expect {
+        "Username:" {send "cloud.admin\r"; exp_continue}
+		"Password:" {send "gReEDy@1Blow\r"; exp_continue}
+		"Retype Password:" {send "gReEDy@1Blow\r"; exp_continue}
+		"Identity domain:" {send "gse00002265\r"; exp_continue}
+       "Region" {send "emea\r"; exp_continue}
+		"Output format:" {send "\r"}
+}
+expect eof
