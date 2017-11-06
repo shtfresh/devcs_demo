d8b03024eb07e42d5124c4aa7e45a98ed1968ad7
shengjun-zh <zhu_sj@126.com>
2017-11-06 10:58:19 +0800

    add debug log

--- sh/update.sh
+++ sh/update.sh
@@ -10,17 +10,7 @@ echo ">>>End psm version...<<<"
 
 echo ">>>Start psm setup1...<<<"
 echo "cloud.admin\ngReEDy@1Blow\ngReEDy@1Blow\ngse00002265\nemea\nshort" | psm setup
-echo ">>>Start psm setup2...<<<"
-echo -e "cloud.admin\ngReEDy@1Blow\ngReEDy@1Blow\ngse00002265\nemea\nshort" | psm setup
-echo ">>>Start psm setup3...<<<"
 
-echo ">>>Start psm setup4...<<<"
-
-echo ">>>End psm setup...<<<"
-
-echo ">>>Start psm update...<<<"
-psm update
-echo ">>>End psm update...<<<"
 
 echo ">>>Start psm help...<<<"
 psm help
