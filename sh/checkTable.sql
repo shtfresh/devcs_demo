3bf9255170cd0d43f3f41e2de168c7f2c5e60557
tasheng <tasheng@TASHENG-CN.cn.oracle.com>
2017-11-09 16:42:07 +0800

    update checkTable.sql

--- sh/checkTable.sql
+++ sh/checkTable.sql
@@ -1,8 +1,11 @@
+set serveroutput on;
 declare
 v_count number;
 begin
+dbms_output.put_line('begin---------');
 select count(1) into v_count from all_tables where TABLE_NAME='EMPLOYEE';
 if v_count > 0 then
+dbms_output.put_line('drop table EMPLOYEE---------');
 execute immediate 'drop table EMPLOYEE';
 end if;
 end;
